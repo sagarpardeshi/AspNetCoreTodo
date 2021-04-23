f4213021f5f824d068f6a8c69622b32e0f62f411
= <=>
2021-04-23 23:30:00 +0530

    Sample Commit

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -14,14 +14,14 @@ namespace AspNetCoreTodo.Controllers
 	public class TodoController : Controller
 	{
         private readonly ILogger<TodoController> _logger;
-
+        
         private readonly ITodoItemService _todoItemService;
 
         public TodoController(ITodoItemService todoItemService)
         {
             _todoItemService = todoItemService;
         }
-
+        
         public async Task<IActionResult> Index()
         {
 			var items = await _todoItemService.GetIncompleteItemsAsync();
